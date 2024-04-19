048f4fa1eceece551be3e7c7c37638dd3b4a9b51
brianonchain <jonwayhuang@gmail.com>
2024-04-19 18:04:19 +0800

    minor

--- frontend/src/app/app/page.tsx
+++ frontend/src/app/app/page.tsx
@@ -472,15 +472,6 @@ const User = () => {
               </div>
             </div>
           )}
-          {menu == "signOut" && (
-            <div>
-              <div className="modal">
-                {/*---content---*/}
-                <div className="mb-8 grow flex flex-col justify-center text-lg md:text-base text-center">{errorMsg}</div>
-              </div>
-              <div className="modalBlackout" onClick={() => setErrorModal(false)}></div>
-            </div>
-          )}
         </div>
       )}
     </div>
