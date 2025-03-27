4c88d5e0564ec1e810176a2374f914a2e4741615
brianonchain <jonwayhuang@gmail.com>
2025-03-27 17:37:27 +0800

    changed keccak256() param type

--- frontend/src/app/api/getPayments/route.ts
+++ frontend/src/app/api/getPayments/route.ts
@@ -13,7 +13,7 @@ export const POST = async (request: NextRequest) => {
   if (nullaInfo.userType === "owner") {
     const prefix = ["0", "2", "4", "6", "8", "a", "c", "e"].includes(w3Info.publicKey.slice(-1)) ? "02" : "03"; // if y is even, then prefix is 02
     const publicKeyCompressed = prefix + w3Info.publicKey.substring(2).slice(0, -64); // substring(2) removes first 2 chars, slice(0, -64) removes last 64 chars
-    var merchantEvmAddress = getAddress("0x" + keccak256(Buffer.from(w3Info.publicKey.substring(2), "hex")).slice(-40)); // slice(-40) keeps last 40 chars
+    var merchantEvmAddress = getAddress("0x" + keccak256(("0x" + w3Info.publicKey.substring(2)) as `0x${string}`).slice(-40)); // slice(-40) keeps last 40 chars
     const jwks = createRemoteJWKSet(new URL("https://api-auth.web3auth.io/jwks")); // for social logins
     const jwtDecoded = await jwtVerify(w3Info.idToken, jwks, { algorithms: ["ES256"] });
     verified = (jwtDecoded.payload as any).wallets[2].public_key.toLowerCase() === publicKeyCompressed.toLowerCase();
